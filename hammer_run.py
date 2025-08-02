13fb7ed548c3eb206187e9d486814571a21731a9
ntini773 <grandhi.sai@research.iiit.ac.in>
2025-08-03 02:14:17 +0530

    Added HAMMER.md for documenting

--- hammer_run.py
+++ hammer_run.py
@@ -2,7 +2,7 @@ import hammer, pathlib, sys
 
 # print(dir(hammer))
 # elf = sys.argv[1]
-elf = "/home/nitin/cocotb_simulation/top_tracing_simulation/ibex_arithmetic_basic_test_0.o"
+elf = "./ibex_load_instr_test_0.o"
 
 # Build one mem_cfg_t object – (base, size) just like the C++ struct
 mem_cfg = hammer.mem_cfg_t(0x8000_0000, 256 * 1024 * 1024)   # 256 MiB DRAM
@@ -14,6 +14,7 @@ sim = hammer.Hammer("RV32IMC", "msu",   "",   [0],  [mem_cfg],elf,None)
 #                ↑          ↑           ↑     ↑       ↑           ↑     ↑
 #                isa   privilege_levels  vlen hartIDs  mem_layout  ELF  (start_pc optional)
 
+print("Simulator Instantiated")
 # print("Initial PC:", hex(sim.get_PC(0)))
 # for i in range(10):
 #     sim.single_step(0)
